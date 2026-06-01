f696b6507a7afa9d464fd53c5525c5a5f5a62c0f
lazykeys <155578589+lazykeys@users.noreply.github.com>
2026-06-01 17:29:54 -0400

    In-Progress: Experience Items

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,27 @@
-export function Button({ variant = "primary", ...props })
+import { twMerge } from "tailwind-merge";
+
+export function Button({ variant = "primary", className, ...props })
 {
-    return <button {...props} className={`${getVariantStyles(variant)}`}/>
+    return (
+        <button 
+            {...props} 
+            className={twMerge(
+                "text-base text-lk-6 bg-lk-3 hover:bg-lk-4 font-DigitalDiscoRegular rounded transition-colors",
+                getVariantStyles(variant),
+                className)
+            }           
+        />
+    )
 }
 
 function getVariantStyles(variant)
 {
     switch (variant) {
         case "primary":
-            return "text-xl text-lk-6 bg-lk-3 hover:bg-lk-4 w-full font-DigitalDiscoRegular h-16 rounded transition-colors"
+            return "text-xl text-lk-6 w-full font-DigitalDiscoRegular h-16"
             break;
         case "tabular":
-            return "text-2xl text-lk-6 bg-lk-3 hover:bg-lk-4 w-1/3 px-2 py-1 font-DigitalDiscoThin rounded transition-colors"
+            return "text-2xl text-lk-6 w-1/3 font-DigitalDiscoThin rounded-lg"
             break;
         case "svg":
             return "fill-lk-3 hover:fill-lk-4 transition-colors"
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,23 @@
-import { Button } from "./Button"
-import { Svg } from "./Svg"
-import usfLogo from "/src/assets/images/usf_logo.webp"
+import { Button } from "./Button";
+import { Svg } from "./Svg";
+import experiences from "../assets/data/experiences.json"
 
 export function Experience() {
     return (
         <div className="flex flex-col gap-3">
             <Tabs/>
-            <section className="border-5 border-lk-3 rounded-xl w-full h-128 overflow-auto scrollbar-none">
-                <ExperienceItem/>
-                <ExperienceItem/>
-                <ExperienceItem/>
-                <ExperienceItem/>
+            <section className="border-5 border-lk-3 rounded-xl w-full h-fit">
+                {experiences.work.map(experience => (
+                    <ExperienceItem 
+                        key={experience.id}
+                        iconPath={experience.iconPath}
+                        header={experience.employer}
+                        subheader={experience.position}
+                        startDate={experience.startDate}
+                        endDate={experience.endDate}
+                        notes={experience.notes}
+                    />
+                ))}
             </section>
         </div>
     )
@@ -18,7 +25,7 @@ export function Experience() {
 
 function Tabs() {
     return (
-        <div className="flex w-full justify-around items-center px-1 border-7 border-lk-3 rounded-xl bg-lk-3">
+        <div className="flex w-full justify-around items-center border-7 border-lk-3 p-0.5 rounded-xl bg-lk-3">
             <Button variant="tabular">Work</Button>
             <Button variant="tabular">Education</Button>
             <Button variant="tabular">Projects</Button>
@@ -26,23 +33,30 @@ function Tabs() {
     )
 }
 
-function ExperienceItem() {
+function ExperienceItem({ iconPath, header, subheader, startDate, endDate, notes }) {
     return (
         <div className="flex">
             <div className="flex flex-col m-3">
-                <img className="mask-radial-at-center mask-radial-from-70% mask-radial-to-70% mask-circle min-w-14 w-14 bg-white" src={usfLogo}/>
+                <img className="mask-radial-at-center mask-radial-from-70% mask-radial-to-70% mask-circle min-w-14 w-14 bg-white" src={iconPath}/>
                 <div className="flex h-full">
-                    <div className="border-r-2 border-lk-2 w-1/2 my-3"></div>
-                    <div className="border-l-2 border-lk-2 w-1/2 my-3"></div>
+                    <div className="border-r-2 border-lk-2 w-1/2 mb-2"></div>
+                    <div className="border-l-2 border-lk-2 w-1/2 mb-2"></div>
                 </div>
             </div>
-            <div className="flex flex-col m-3">
-                <h1 className="text-xl text-lk-6 font-DigitalDiscoRegular">Employer</h1>
-                <h2 className="text-lg text-lk-6 font-DigitalDiscoThin">Position</h2>
+            <div className="flex flex-col m-3 w-full">
+                <h1 className="text-xl text-lk-6 font-DigitalDiscoRegular">{header}</h1>
+                <div className="flex justify-between">
+                    <h2 className="text-lg text-lk-6 font-DigitalDiscoThin">{subheader}</h2>
+                    <div className="flex w-fit justify-between items-center gap-1">
+                        <h3 className="text-md text-lk-6 font-DigitalDiscoThin">{startDate}</h3>
+                        <h3 className="text-md text-lk-6 font-DigitalDiscoThin">-</h3>
+                        <h3 className="text-md text-lk-6 font-DigitalDiscoThin">{endDate}</h3>
+                    </div>
+                </div>
                 <ul className="list-disc">
-                    <li className="text-base ml-3 text-lk-6 font-DigitalDiscoThin">Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.</li>
-                    <li className="text-base ml-3 text-lk-6 font-DigitalDiscoThin">Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.</li>
-                    <li className="text-base ml-3 text-lk-6 font-DigitalDiscoThin">Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.</li>
+                    {notes.map(note => (
+                        <li className="text-base ml-3 text-lk-6 font-DigitalDiscoThin">{note}</li>
+                    ))}
                 </ul>
             </div>
         </div>
